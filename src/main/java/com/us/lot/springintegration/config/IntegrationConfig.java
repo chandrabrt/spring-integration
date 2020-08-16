b8efd787950aa63dbf3b9ed8d3bc7f443ecb8549
chandra khadka <chandra2khadka4@gmail.com>
2020-08-16 10:19:40 +0545

    ES-0001: transfer

--- src/main/java/com/us/lot/springintegration/config/IntegrationConfig.java
+++ src/main/java/com/us/lot/springintegration/config/IntegrationConfig.java
@@ -1,10 +1,16 @@
 package com.us.lot.springintegration.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.us.lot.springintegration.resource.Student;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.json.JsonToObjectTransformer;
+import org.springframework.integration.json.ObjectToJsonTransformer;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
 import org.springframework.messaging.MessageChannel;
 
 /**
@@ -31,5 +37,25 @@ public class IntegrationConfig {
     @Bean
     public MessageChannel replyChannel(){
         return new DirectChannel();
+    }
+
+    //transformer
+    @Bean
+    @Transformer(inputChannel = "integration.student.gateway.channel",
+            outputChannel = "integration.student.objectToJson.channel")
+    public ObjectToJsonTransformer objectToJsonTransformer(){
+        return new ObjectToJsonTransformer(getMapper());
+    }
+
+    @Bean
+    public Jackson2JsonObjectMapper getMapper() {
+        return new Jackson2JsonObjectMapper(new ObjectMapper());
+    }
+
+    @Bean
+    @Transformer(inputChannel = "integration.student.jsonToObject.channel",
+            outputChannel = "integration.student.jsonToObject.fromTransformer.channel")
+    public JsonToObjectTransformer jsonToObjectTransformer(){
+        return new JsonToObjectTransformer(Student.class);
     }
 }
